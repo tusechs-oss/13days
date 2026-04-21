85fd7f09f4fb05061398b5f87daf2afaf148f737
Nguyen Anh Tu <anhtu1992000@gmail.com>
2026-04-21 22:36:12 +0700

    fix

--- player.gd
+++ player.gd
@@ -68,24 +68,16 @@ func check_pickup():
         
         if target and target.is_in_group("items"):
             picked_item = target 
-            
-            # 1. Đóng băng vật lý và tắt va chạm ngay lập tức
+        
             picked_item.freeze = true
             picked_item.collision_layer = 0
             picked_item.collision_mask = 0
-            
-            # 2. Dùng reparent để đưa vào 'hand' (node con của hands_cam)
-            # Dùng lệnh này nhanh và an toàn hơn remove_child/add_child
             picked_item.reparent(hand)
-            
-            # 3. Đưa về tâm của node 'hand'
-            # Tú chỉ cần để 2 dòng này là ra ngoài Inspector xoay node 'hand' thoải mái
             picked_item.position = Vector3.ZERO
             picked_item.rotation = Vector3.ZERO
             picked_item.scale = Vector3(0.03, 0.03, 0.03)
 
-            # 4. Chuyển Layer hiển thị (Gộp 2 vòng lặp của Tú thành 1 cho gọn)
             for child in picked_item.get_children():
                 if child is VisualInstance3D:
-                    child.set_layer_mask_value(1, false) # Tắt layer chính
-                    child.set_layer_mask_value(2, true)  # Bật layer phụ
+                    child.set_layer_mask_value(1, false)
+                    child.set_layer_mask_value(2, true)
